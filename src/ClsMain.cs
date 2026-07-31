dba43b30546ca75c6cd4dbcea0ab02c23683011f
Cristofer Candelas Garcia <cristofer.candelas.garcia@gmail.com>
2026-07-30 20:50:57 -0600

    Bump version to 2.50.0

--- src/ClsMain.cs
+++ src/ClsMain.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
-[assembly: AssemblyVersion("2.49.0.0")]
+[assembly: AssemblyVersion("2.50.0.0")]
 [assembly: AssemblyTitle("BrosLMV - Botones CONTPAQi")]
 
 namespace BrosLMV
